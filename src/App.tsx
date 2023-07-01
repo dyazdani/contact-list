5f51325e3d2438e531a05df76ea55e11d8f4cd6f
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-01 12:44:13 -0700

    Added content to Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Home from './components/Home'
 
 
 function App() {
 
   return (
     <>
-      
+      <Home />
     </>
   )
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,37 @@
+import React, {useEffect} from "react";
+//TODO: How to store number of contacts in a way for this component to read it? UseState? 
+//TODO: fix CORS policy error in fetch call
+
+
+
+
+const Home = () => {
+    const numberOfContacts = 0;
+
+    const fetchCurrentNumberOfContacts = async () => {
+        const response = await fetch('https://my-json-server.typicode.com/dyazdani/contact-list', {
+            method: "GET",
+
+        });
+        const data = response.json();
+        console.log(data)
+        //TODO: figure out what part of data will yield number of contacts
+    }
+
+    useEffect(() => {
+        fetchCurrentNumberOfContacts();
+    }, [])
+
+    return (
+        <div>
+            <h1>Welcome to UrContacts!</h1>
+            <h2>Your list has {numberOfContacts} contacts</h2>
+            <a>Go to UrContacts List</a>
+        </div>
+    )
+
+
+}
+
+
+export default Home;
