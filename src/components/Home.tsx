11357e36fcc462766caf85a41731ffcdf3fabf70
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-03 13:52:02 -0700

    Made changes to Home.tsx so that number of contacts in list is stored with useState hook and displayed in component when rendered

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,13 @@
-import {useEffect} from "react";
-//TODO: How to store number of contacts in a way for this component to read it? UseState? 
-//TODO: fix CORS policy error in fetch call
-
-
+import {useEffect, useState} from "react";
 
 
 const Home = () => {
-    const numberOfContacts = 0;
+    const [numberOfContacts, setNumberOfContacts] = useState(0);
 
     const fetchJsonData = async () => {
-        const response = await fetch('http://localhost:3000/posts');
+        const response = await fetch('http://localhost:3000/contacts');
         const data = await response.json();
-        console.log(data)
-        //TODO: figure out what part of data will yield number of contacts
+        setNumberOfContacts(data.length);
     }
 
     useEffect(() => {
