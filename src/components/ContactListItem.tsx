15c744da44baa96c226f0bac37c74e20e78b5e66
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-05 11:04:59 -0700

    Boilerplate code for ContactListItem.tsx

---
+++ src/components/ContactListItem.tsx
@@ -0,0 +1,14 @@
+//TODO: link this component to ContactDetails.tsx page with details corresponding to name prop
+
+const ContactListItem = ({name}) => {
+
+    return (
+        <li>
+            <a href="">{name}</a>
+        </li>
+    )
+
+}
+
+
+export default ContactListItem;
