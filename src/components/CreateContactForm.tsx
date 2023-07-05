b8a013a2b9b1a4e025c8a5340667122e643d41f5
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-05 11:54:26 -0700

    Boilerplate code for CreateContactForm.tsx

---
+++ src/components/CreateContactForm.tsx
@@ -0,0 +1,37 @@
+const CreateContactForm = () => {
+
+    return (
+        <form>
+            <input placeholder="First Name" type="text" />
+            <input placeholder="Last Name" type="text" />
+            <label htmlFor="gender-select">Gender:</label>
+            <select name="gender" id="gender-select">
+                <option value="">--Please choose an option--</option>
+                <option value="female">Female</option>
+                <option value="male">Male</option>
+                <option value="prefer-to-not-say">Prefer to not say</option>
+            </select>
+            <input name="birthday" type="date" />
+            <input type="email" />
+            <input type="tel" 
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
+                placeholder="ex: 444-444-4444" />
+            <input type="number" placeholder="country code"/>
+            <div id="address">
+                <p>Address:</p>
+                <input type="text" placeholder="Street"/>
+                <input type="text" placeholder="Unit"/>
+                <input type="text" placeholder="City"/>
+                <input type="text" placeholder="State"/>
+                <input type="text" placeholder="ZIP Code"/>
+                <input type="text" placeholder="Country"/>
+            </div>
+            <button>Submit</button>
+        </form>
+    )
+
+
+}
+
+
+export default CreateContactForm;
