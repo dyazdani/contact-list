93c878f29f73bcb27c66647f3566f38994d453aa
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-05 11:11:56 -0700

    Boilerplate code for DeleteContctButton.tsx

---
+++ src/components/DeleteContactButton.tsx
@@ -0,0 +1,12 @@
+import handleDeleteButtonClick from "./../utilities";
+
+const DeleteContactButton = () => {
+
+    return (
+        <button type="button" onClick={handleDeleteButtonClick}>Delete Contact</button>
+    )
+
+}
+
+
+export default DeleteContactButton;
