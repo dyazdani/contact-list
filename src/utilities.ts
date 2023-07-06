a7f67821b851d90872b68ee4199fa261cf7f6012
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-06 11:15:30 -0700

    Deleted getContacts function

--- src/utilities.ts
+++
@@ -1,8 +0,0 @@
-
-const fetchContacts = async () => {
-    const response = await fetch('https://localhost:3000');
-    const contact = await response.json();
-    return contact;
-}
-
-export default fetchContacts;
