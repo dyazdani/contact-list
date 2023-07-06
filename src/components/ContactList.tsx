24d130bd8ad280f8ae4166d6d5a94e2be0c35fae
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-06 11:13:28 -0700

    Added fetchContacts function to ContactList.tsx

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -1,7 +1,18 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const ContactList = () => {
+    const [contacts, setContacts] = useState(null);
+    
+    const fetchContacts = async () => {
+        const response = await fetch('http://localhost:3000/contacts');
+        const contactsData = await response.json();
+        setContacts(contactsData);
+    }
 
+    useEffect(() => {
+        fetchContacts();
+    }, []) 
     return (
         <>
             <ul>Test</ul>
