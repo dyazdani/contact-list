58740c6a3919c23c2ab11804f0f5396ce4f67064
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-09 06:53:12 -0700

    Used useState hook to store and use values in UpdateContactForm.tsx

--- src/components/UpdateContactForm.tsx
+++ src/components/UpdateContactForm.tsx
@@ -1,49 +1,23 @@
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-/*
-TYPES:
-    firstName: string
-    lastName: string
-    gender?: string
-    birthday?: number
-    email?: `${string}@${string}.${string}`
-    phoneNumber?: number
-    countryCode?: number
-    street?: `${number} ${string}`
-    unit?: string
-    city?: string
-    state?: string
-    zip?: number
-    country?: string
-
-*/
-
-const blankContact = {
-    id: null,
-    name: "",
-    gender: null,
-    birthday: "",
-    email: "",
-    phone: {
-        phone: "",
-        country_code: ""
-    },
-    address: {
-        street: "",
-        unit: "",
-        city: "",
-        state: "",
-        zip: "",
-        country: ""
-    }
-}
-
 const UpdateContactForm = () => {
-    //TODO: use variables to store boolean value for selection option chosen in db
-    //TODO: use a get request to get data and then populate the form fields with those data values.
-    //TODO: PUT request to update the existing data with data in the request
-    const [currentContact, setCurrentContact] = useState(blankContact);
+    //TODO: control forms
+    //TODO: fix warning that says date string is not in correct format
+    //TODO: POST request place edited contact in database
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [gender, setGender] = useState("");
+    const [birthday, setBirthday] = useState("");
+    const [email, setEmail] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [countryCode, setCountryCode] = useState("");
+    const [street, setStreet] = useState("");
+    const [unit, setUnit] = useState("");
+    const [city, setCity] = useState("");
+    const [province, setProvince] = useState("");
+    const [zip, setZip] = useState("");
+    const [country, setCountry] = useState("");
 
     const location = useLocation();
     const currentId = location.state;
@@ -51,32 +25,47 @@ const UpdateContactForm = () => {
     const fetchContacts = async () => {
         const response = await fetch('http://localhost:3000/contacts');
         const contactsData = await response.json();
-        console.log('Contacts:', contactsData)
+        // console.log('Contacts:', contactsData)
         for (let i = 0; i < contactsData.length; i++) {
-            console.log(contactsData[i].id, contactsData[i].id == currentId)
+            // console.log(contactsData[i].id, contactsData[i].id == currentId)
             if (contactsData[i].id === currentId) {
-                setCurrentContact(contactsData[i]);
+                const currentContact = contactsData[i];
+
+                console.log(currentContact);
+                
+                const nameArray = (currentContact.name).split(" ");
+                setFirstName(nameArray[0]);
+                setLastName(nameArray[1]);
+
+                setGender(currentContact.gender);
+
+                const birthdayYear = new Date(currentContact.birthday).getFullYear();
+                const birthdayMonth = new Date(currentContact.birthday).getMonth();
+                const birthdayDate = new Date(currentContact.birthday).getDate();
+                setBirthday(`${birthdayYear}-${birthdayMonth}-${birthdayDate}`);
+
+                setEmail(currentContact.email);
+
+                const rawPhoneString = currentContact.phone.number.toString();
+                setPhoneNumber(`${rawPhoneString.slice(0,3)}-${rawPhoneString.slice(3, 6)}-${rawPhoneString.slice(6)}`);
+                
+                setCountryCode(currentContact.phone.country_code);
+                setStreet(currentContact.address.street);
+                setUnit(currentContact.address.unit);
+                setCity(currentContact.address.city);
+                setProvince(currentContact.address.province);
+                setZip(currentContact.address.zip);
+                setCountry(currentContact.address.country);
+
                 return;
             }
         }
-        console.log(currentContact)
+        // console.log(currentContact)
     }
 
     useEffect(() => {
         fetchContacts();
     }, [])
-
-    // Breaking up the data to populate the form inputs
-    const nameArray = (currentContact.name).split(" ");
-    const firstName = nameArray[0];
-    const lastName = nameArray[1];
-
-    const birthdayYear = new Date(currentContact.birthday).getFullYear();
-    const birthdayMonth = new Date(currentContact.birthday).getMonth();
-    const birthdayDate = new Date(currentContact.birthday).getDate();
-
-    const rawPhoneString = currentContact.phone.phone.toString();
-    const phoneNumber = `${rawPhoneString.slice(0,3)}-${rawPhoneString.slice(3, 6)}-${rawPhoneString.slice(6)}`
 
     return (
         <>
@@ -85,21 +74,18 @@ const UpdateContactForm = () => {
                 <input placeholder="First Name" type="text" value={firstName} />
                 <input placeholder="Last Name" type="text" value={lastName} />
                 <label htmlFor="gender-select">Gender:</label>
-                <select name="gender" id="gender-select">
-                    <option value="">--Please choose an option--</option>
-                    <option value="female" selected={currentContact.gender === "female"}>Female</option>
-                    <option value="male" selected={currentContact.gender === "male"}>Male</option>
-                    <option value="non-binary" selected={currentContact.gender === "non-binary"}>Non-Binary</option>
-                    <option value="other" selected={currentContact.gender === "other"}>Other</option>
-                    <option 
-                        value="prefer-to-not-say" 
-                        selected={currentContact.gender === "prefer-not-to-say"}>Prefer to not say</option>
+                <select name="gender" id="gender-select" defaultValue={gender}>
+                    <option value="female">Female</option>
+                    <option value="male">Male</option>
+                    <option value="non-binary">Non-Binary</option>
+                    <option value="other">Other</option>
+                    <option value="prefer-to-not-say">Prefer to not say</option>
                 </select>
                 <input 
                     name="birthday" 
                     type="date" 
-                    value={`${birthdayYear}-${birthdayMonth}-${birthdayDate}`} />
-                <input type="email" value={currentContact.email}/>
+                    value={birthday} />
+                <input type="email" value={email}/>
                 <input 
                     type="tel" 
                     pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
@@ -108,15 +94,15 @@ const UpdateContactForm = () => {
                 <input 
                     type="number" 
                     placeholder="country code" 
-                    value={currentContact.phone.country_code}/>
+                    value={countryCode}/>
                 <div id="address">
                     <p>Address:</p>
-                    <input type="text" placeholder="Street" value={currentContact.address.street}/>
-                    <input type="text" placeholder="Unit" value={currentContact.address.unit}/>
-                    <input type="text" placeholder="City" value={currentContact.address.city}/>
-                    <input type="text" placeholder="State" value={currentContact.address.state}/>
-                    <input type="text" placeholder="ZIP Code" value={currentContact.address.zip}/>
-                    <input type="text" placeholder="Country" value={currentContact.address.country}/>
+                    <input type="text" placeholder="Street" value={street}/>
+                    <input type="text" placeholder="Unit" value={unit}/>
+                    <input type="text" placeholder="City" value={city}/>
+                    <input type="text" placeholder="State" value={province}/>
+                    <input type="text" placeholder="ZIP Code" value={zip}/>
+                    <input type="text" placeholder="Country" value={country}/>
                 </div>
                 <button>Submit</button>
             </form>
