ff3cfe677cab84448e3c5b27d8f2826c0828b3bf
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-10 07:03:05 -0700

    Fixed bug that gave error about component value changing from defined to undefined

--- src/components/UpdateContactForm.tsx
+++ src/components/UpdateContactForm.tsx
@@ -115,20 +115,20 @@ const UpdateContactForm = () => {
                 <input 
                     placeholder="First Name" 
                     type="text" 
-                    value={firstName} 
+                    value={firstName ?? ""} 
                     onChange={(e) => setFirstName(e.target.value)}
                 />
                 <input 
                     placeholder="Last Name" 
                     type="text" 
-                    value={lastName} 
+                    value={lastName ?? ""} 
                     onChange={(e) => setLastName(e.target.value)}
                 />
                 <label htmlFor="gender-select">Gender:</label>
                 <select 
                     name="gender" 
                     id="gender-select" 
-                    value={gender}
+                    value={gender ?? ""}
                     onChange={(e) => setGender(e.target.value)}
                 >
                     <option value="female">Female</option>
@@ -140,25 +140,25 @@ const UpdateContactForm = () => {
                 <input 
                     name="birthday" 
                     type="date" 
-                    value={birthday} 
+                    value={birthday ?? ""} 
                     onChange={(e) => setBirthday(e.target.value)}
                 />
                 <input 
                     type="email" 
-                    value={email}
+                    value={email ?? ""}
                     onChange={(e) => setEmail(e.target.value)}
                 />
                 <input 
                     type="tel" 
                     pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
                     placeholder="ex: 444-444-4444" 
-                    value={phoneNumber}
+                    value={phoneNumber ?? ""}
                     onChange={(e) => setPhoneNumber(e.target.value)}
                 />
                 <input 
                     type="number" 
                     placeholder="country code" 
-                    value={countryCode}
+                    value={countryCode ?? ""}
                     onChange={(e) => setCountryCode(e.target.value)}
                 />
                 <div id="address">
@@ -166,37 +166,37 @@ const UpdateContactForm = () => {
                     <input 
                         type="text" 
                         placeholder="Street" 
-                        value={street}
+                        value={street ?? ""}
                         onChange={(e) => setStreet(e.target.value)}
                     />
                     <input 
                         type="text" 
                         placeholder="Unit" 
-                        value={unit}
+                        value={unit ?? ""}
                         onChange={(e) => setUnit(e.target.value)}
                     />
                     <input 
                         type="text" 
                         placeholder="City" 
-                        value={city}
+                        value={city ?? ""}
                         onChange={(e) => setCity(e.target.value)}
                     />
                     <input 
                         type="text" 
                         placeholder="State" 
-                        value={province}
+                        value={province ?? ""}
                         onChange={(e) => setProvince(e.target.value)}
                     />
                     <input 
                         type="text" 
                         placeholder="ZIP Code" 
-                        value={zip}
+                        value={zip ?? ""}
                         onChange={(e) => setZip(e.target.value)}
                     />
                     <input 
                         type="text" 
                         placeholder="Country" 
-                        value={country}
+                        value={country ?? ""}
                         onChange={(e) => setCountry(e.target.value)}
                     />
                 </div>
