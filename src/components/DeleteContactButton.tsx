6587626d747296fdbdfa902955359539b63d4883
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-11 21:49:21 -0700

    Fixed bug in DELETE request in DeleteContactButton.tsx

--- src/components/DeleteContactButton.tsx
+++ src/components/DeleteContactButton.tsx
@@ -1,15 +1,12 @@
-import { useEffect } from "react";
-
 interface DeleteButtonProps {
     id: string
 }
 
 const DeleteContactButton = ({id}: DeleteButtonProps) => {
-    //TODO: get this DELETE request to work
     const handleDeleteButtonClick = async () => {
         console.log('Contacting server...')
         try {
-            const response = await fetch(`http://locoalhost:3000/contacts/${id}`, {method: "DELETE"});
+            const response = await fetch(`http://localhost:3000/contacts/${id}`, {method: "DELETE"});
             const result = await response.json();
             console.log("UrContact was successfully deleted", result);
         } catch (error) {
