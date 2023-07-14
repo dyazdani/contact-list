abb44d55ac49958059ce4fccb9072fe2e5b6c7aa
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-14 10:47:55 -0700

    Added useNavigate hook to route back to contact list after deleting a contact

--- src/components/DeleteContactButton.tsx
+++ src/components/DeleteContactButton.tsx
@@ -1,14 +1,18 @@
+import { useNavigate } from "react-router-dom"
+
 interface DeleteButtonProps {
     id: string
 }
 
 const DeleteContactButton = ({id}: DeleteButtonProps) => {
+    const navigate = useNavigate();
     const handleDeleteButtonClick = async () => {
         console.log('Contacting server...')
         try {
             const response = await fetch(`http://localhost:3000/contacts/${id}`, {method: "DELETE"});
             const result = await response.json();
             console.log("UrContact was successfully deleted", result);
+            navigate("/contacts");
         } catch (error) {
             console.log('Failure:', error);
         }
