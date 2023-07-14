a1ccfb2dd272932176483e93eac3e321f32e5cbd
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-14 12:55:24 -0700

    Added labels to CreateContactForm.tsx

--- src/components/CreateContactForm.tsx
+++ src/components/CreateContactForm.tsx
@@ -64,15 +64,19 @@ const CreateContactForm = () => {
 
     return (
         <form onSubmit={handleSubmit}>
+            <label htmlFor="first-name">First Name: </label>
             <input 
                 placeholder="First Name" 
                 type="text" 
+                id="first-name"
                 value={firstName} 
                 onChange={(e) => setFirstName(e.target.value)}
             />
+            <label htmlFor="last-name">Last Name: </label>
             <input 
                 placeholder="Last Name" 
-                type="text" 
+                type="text"
+                id="last-time" 
                 value={lastName} 
                 onChange={(e) => setLastName(e.target.value)}
             />
@@ -90,55 +94,76 @@ const CreateContactForm = () => {
                 <option value="other">Other</option>
                 <option value="prefer-to-not-say">Prefer to not say</option>
             </select>
+            <label htmlFor="birthday">Birthday: </label>
             <input 
                 name="birthday" 
                 type="date" 
+                id="birthday"
                 value={birthday} 
                 onChange={(e) => setBirthday(e.target.value)}/>
+            <label htmlFor="email">Email: </label>
             <input 
+                placeholder="ex: bob@bobmail.com"
                 type="email" 
                 value={email} 
+                id="email"
                 onChange={(e) => setEmail(e.target.value)}/>
+            <label htmlFor="phone-number">Phone Number: </label>
             <input type="tel" 
                 pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
                 placeholder="ex: 444-444-4444" 
+                id="phone-number"
                 value={phoneNumber} 
                 onChange={(e) => setPhoneNumber(e.target.value)}/>
+            <label htmlFor="country-code">Country Code: </label>
             <input 
                 type="number" 
                 placeholder="country code"
+                id="country-code"
                 value={countryCode} 
                 onChange={(e) => setCountryCode(e.target.value)}/>
             <div id="address">
                 <p>Address:</p>
+                <label htmlFor="street">Street: </label>
                 <input 
                     type="text" 
                     placeholder="Street"
+                    id="street"
                     value={street} 
                     onChange={(e) => setStreet(e.target.value)}/>
+                <label htmlFor="unit">Unit: </label>
                 <input 
                     type="text" 
                     placeholder="Unit"
+                    id="unit"
                     value={unit} 
                     onChange={(e) => setUnit(e.target.value)}/>
+                <label htmlFor="city">City: </label>
                 <input 
                     type="text" 
                     placeholder="City"
+                    id="city"
                     value={city} 
                     onChange={(e) => setCity(e.target.value)}/>
+                <label htmlFor="state">State: </label>
                 <input 
                     type="text" 
                     placeholder="State"
+                    id="state"
                     value={province} 
                     onChange={(e) => setProvince(e.target.value)}/>
+                <label htmlFor="zip-code">ZIP Code: </label>
                 <input
                     type="text" 
                     placeholder="ZIP Code"
+                    id="zip-code"
                     value={zip} 
                     onChange={(e) => setZip(e.target.value)}/>
+                <label htmlFor="country">Country: </label>
                 <input 
                     type="text" 
                     placeholder="Country"
+                    id="country"
                     value={country} 
                     onChange={(e) => setCountry(e.target.value)}/>
             </div>
